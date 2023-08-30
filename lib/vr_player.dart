90723b9572f37a5af4dd92e51b866245f3b24197
Alesia <alesya.r.03@gmail.com>
2023-08-30 11:51:56 +0300

    dart format to get highest pana score

--- lib/vr_player.dart
+++ lib/vr_player.dart
@@ -1,7 +1,7 @@
 /// The `VrPlayer` plugin lets you play 360° and VR videos smoothly on Android
-/// and iOS platforms, delivering an immersive viewing experience with touch 
+/// and iOS platforms, delivering an immersive viewing experience with touch
 /// and device motion controls. These types of videos, commonly referred to as
-/// immersive, 360, or spherical videos, are captured by utilizing 
+/// immersive, 360, or spherical videos, are captured by utilizing
 /// an omnidirectional camera or multiple cameras to record
 /// the entire panoramic view simultaneously.
 library vr_player;
